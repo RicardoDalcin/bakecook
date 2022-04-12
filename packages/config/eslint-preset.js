da5e6124aef150e8442585e2b7808fa1f43fc078
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2022-04-11 23:38:00 -0300

    🔧 Add basic ESLint and EditorConfig configuration

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,12 +1,76 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
+      rootDir: ['apps/*/', 'packages/*/'],
+    },
+    react: {
+      version: 'detect',
     },
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'prettier/prettier': 'error',
+
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+
+    'no-unused-vars': 'off',
+    'dot-notation': 'off',
+
+    camelcase: 'off',
+    'class-methods-use-this': 'off',
+    'no-nested-ternary': 'off',
+    'no-use-before-define': 'off',
+    'space-before-function-paren': 'off',
+
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/display-name': 'off',
+    'arrow-parens': 'off',
+
+    'consistent-return': 'warn',
+    'no-return-await': 'off',
+
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
   },
 };
